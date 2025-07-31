3f25fc224f602a052d21539e3b0c3c88bc7d39f0
tharushaudana <tharushaudana519@gmail.com>
2025-07-31 23:27:57 +0530

    demo.mp4 added

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from utils.maze import Maze
 from utils.algo import DFS, BFS, GBFS, ASTAR
 
-maze = Maze("maze5.txt")
+maze = Maze("maze2.txt")
 
+# algo = DFS()
+# algo = BFS()
+# algo = GBFS()
 algo = ASTAR()
-algo.delay = 0.1
+
+algo.delay = 0.01
 
 maze.init()
 
